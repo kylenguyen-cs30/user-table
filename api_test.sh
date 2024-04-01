470311da4b6267f0c7b21b153fa267ad857186e0
Kyle Nguyen <knguyen1193@outlook.com>
2024-03-31 19:46:22 -0700

    test and debug http api

---
+++ api_test.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+# API URL
+
+echo "Creating a new user..."
+http POST localhost:3001/users fName="John" lName="Doe" email="john.doe@example.com" phoneNum="1234567890"
+
+echo "Retrieving all users..."
+http GET localhost:3001/users
+
+# Replace :id with the actual ID of the user you want to interact with in the following commands
+id=":id"
+
+echo "Retrieving a single user by ID..."
+http GET localhost:3001/users/$id
+
+echo "Updating a user..."
+http PUT localhost:3001/users/$id fName="Jane" lName="Doe" email="jane.doe@example.com" phoneNum="0987654321"
+
+echo "Deleting a user..."
+http DELETE localhost:3001/users/$id
