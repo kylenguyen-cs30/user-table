777bd09debf8ded22dfea8c4594520197e2a22ae
Kyle Nguyen <knguyen1193@outlook.com>
2024-03-31 20:18:48 -0700

    change apt_test.sh

--- api_test.sh
+++ api_test.sh
@@ -1,15 +1,22 @@
 #!/bin/zsh
 
-# API URL
 
 echo "Creating a new user..."
 http POST localhost:3001/users fName="John" lName="Doe" email="john.doe@example.com" phoneNum="1234567890"
+
+echo "Creating a new user..."
+http POST localhost:3001/users fName="Jack" lName="Sparrow" email="jack.sparrow@example.com" phoneNum="1234567890"
+
+echo "Creating a new user..."
+http POST localhost:3001/users fName="Captain" lName="Hook" email="captain.hookcaptainmple.com" phoneNum="1234567890"
+
+
 
 echo "Retrieving all users..."
 http GET localhost:3001/users
 
 # Replace :id with the actual ID of the user you want to interact with in the following commands
-id=":id"
+id=2
 
 echo "Retrieving a single user by ID..."
 http GET localhost:3001/users/$id
